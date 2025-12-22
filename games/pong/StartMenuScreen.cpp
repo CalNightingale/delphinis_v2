d59567e10116fceff4714e60cd94bc34e0fc868d
Cal Nightingale <cal.nightingale@gmail.com>
2025-12-22 17:31:20 -0500

    reduced start game text size

--- games/pong/StartMenuScreen.cpp
+++ games/pong/StartMenuScreen.cpp
@@ -25,7 +25,7 @@ void StartMenuScreen::onEnter() {
     // Create instructions text
     Entity instructionsText = getWorld().createEntity();
     getWorld().addComponent(instructionsText, Transform{0.0f, -2.0f});
-    getWorld().addComponent(instructionsText, Text{"Press SPACE to start", Vec3{0.7f, 0.7f, 0.7f}, 2.0f, TextAlign::Center});
+    getWorld().addComponent(instructionsText, Text{"Press SPACE to start", Vec3{0.7f, 0.7f, 0.7f}, 1.0f, TextAlign::Center});
 }
 
 void StartMenuScreen::update(float deltaTime) {
